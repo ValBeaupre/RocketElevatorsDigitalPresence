9421b8068157526b70e86c85b60e8e388b5c6618
ValBeaupre <valbeaupre@hotmail.com>
2018-09-04 21:46:41 -0400

    Corporate Quote (+ others) : done

--- assets/js/quoteForm.js
+++ assets/js/quoteForm.js
@@ -37,7 +37,7 @@ function resQuote() {
 }
 
 function commQuote() {
-  var shafts = document.getElementById("commNbElevShafts").value;
+  var shafts = Number(document.getElementById("commNbElevShafts").value);
   document.getElementById("commNbElevReq").value = shafts;
 
   var rdoStd = document.getElementById("commStandard").checked;
@@ -59,22 +59,40 @@ function commQuote() {
 }
 
 function corpQuote() {
-  var occup = document.getElementById("corpNbOccupants").value;
-  var floors = document.getElementById("corpNbFloors").value;
-  var bsmt = document.getElementById("corpNbBsmtFloors").value;
+  var occup = Number(document.getElementById("corpNbOccupants").value);
+  var floors = Number(document.getElementById("corpNbFloors").value);
+  var bsmt = Number(document.getElementById("corpNbBsmtFloors").value);
   var building = floors + bsmt;
   var totalOccup = occup * building;
   var elvtrs = Math.ceil(totalOccup / 1000);
   var shafts = Math.ceil(building / 20);
-  var elevReq = Math.ceil(elvtrs / shafts);
+  var elevShafts = Math.ceil(elvtrs / shafts);
+  var elevReq = shafts * elevShafts;
 
   document.getElementById("corpNbElevReq").value = elevReq;
+
+  var rdoStd = document.getElementById("corpStandard").checked;
+  var rdoPrem = document.getElementById("corpPremium").checked;
+  var rdoExc = document.getElementById("corpExcelium").checked;
+  var resCorpStd = shafts * 7565 * 1.1;
+  var resCorpPrem = shafts * 12345 * 1.13;
+  var resCorpExc = shafts * 15400 * 1.16;
+
+  if (rdoStd === true) {
+    $("#corpPrice").val(Math.ceil(resCorpStd));
+  }
+  if (rdoPrem === true) {
+    $("#corpPrice").val(Math.ceil(resCorpPrem));
+  }
+  if (rdoExc === true) {
+    $("#corpPrice").val(Math.ceil(resCorpExc));
+  }
 }
 
 function hybQuote() {
-  var occup = document.getElementById("hybNbOccupants").value;
-  var floors = document.getElementById("hybNbFloors").value;
-  var bsmt = document.getElementById("hybNbBsmtFloors").value;
+  var occup = Number(document.getElementById("hybNbOccupants").value);
+  var floors = Number(document.getElementById("hybNbFloors").value);
+  var bsmt = Number(document.getElementById("hybNbBsmtFloors").value);
   var building = floors + bsmt;
   var totalOccup = occup * building;
   var elvtrs = Math.ceil(totalOccup / 1000);
@@ -82,4 +100,21 @@ function hybQuote() {
   var elevReq = Math.ceil(elvtrs / shafts);
 
   document.getElementById("hybNbElevReq").value = elevReq;
+
+  var rdoStd = document.getElementById("hybStandard").checked;
+  var rdoPrem = document.getElementById("hybPremium").checked;
+  var rdoExc = document.getElementById("hybExcelium").checked;
+  var resHybpStd = shafts * 7565 * 1.1;
+  var resHybPrem = shafts * 12345 * 1.13;
+  var resHybExc = shafts * 15400 * 1.16;
+
+  if (rdoStd === true) {
+    $("#corpPrice").val(Math.ceil(resCorpStd));
+  }
+  if (rdoPrem === true) {
+    $("#corpPrice").val(Math.ceil(resCorpPrem));
+  }
+  if (rdoExc === true) {
+    $("#corpPrice").val(Math.ceil(resCorpExc));
+  }
 }
